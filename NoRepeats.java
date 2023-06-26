89cc2c4912b3a08bb275ae6769f7c513f25ae8b3
Vpyc <vpyc4408@gmail.com>
2023-06-26 13:33:59 +0300

    Add RenameVariables

--- NoRepeats.java
+++ NoRepeats.java
@@ -1,25 +1,27 @@
 import java.util.Arrays;
+
 public class NoRepeats {
     public static void main(String[] args) {
-        int n = 8;
-        int arr[] = {5,3,4,5,2,6,1,5};
+        int size = 8;
+        int arr[] = {5, 3, 4, 5, 2, 6, 1, 5};
         System.out.println(Arrays.toString(arr));
-        int temp[] = new int[n];
-        int length = deleteRepeats(arr,temp,n);
-        for(int i = 0; i <length;i++){
-            System.out.print(temp[i] + " ");
+        int newArr[] = new int[size];
+        int length = deleteRepeats(arr, newArr, size);
+        for (int i = 0; i < length; i++) {
+            System.out.print(newArr[i] + " ");
         }
     }
-    public static int deleteRepeats(int[] arr, int[] temp,int n){
+
+    public static int deleteRepeats(int[] arr, int[] temp, int n) {
         int length = 0;
-        for(int i = 0; i<n; i++){
-            boolean f = true;
-            for(int j = 0;j<length;j++){
-                if(temp[j] == arr[i]){
-                    f = false;
+        for (int i = 0; i < n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < length; j++) {
+                if (temp[j] == arr[i]) {
+                    flag = false;
                 }
             }
-            if(f){
+            if (flag) {
                 temp[length] = arr[i];
                 length++;
             }
