c2f812f9dd23f2f3c0828c3fb281d7a2da3ace5a
Vpyc <vpyc4408@gmail.com>
2023-06-26 13:41:02 +0300

    Add Algorithm

---
+++ Algorithm.java
@@ -0,0 +1,18 @@
+public class Algorithm {
+    public static int deleteRepeats(int[] arr, int[] newArr, int n) {
+        int length = 0;
+        for (int i = 0; i < n; i++) {
+            boolean flag = true;
+            for (int j = 0; j < length; j++) {
+                if (newArr[j] == arr[i]) {
+                    flag = false;
+                }
+            }
+            if (flag) {
+                newArr[length] = arr[i];
+                length++;
+            }
+        }
+        return length;
+    }
+}
--- NoRepeats.java
+++ NoRepeats.java
@@ -6,26 +6,9 @@ public class NoRepeats {
         int arr[] = {5, 3, 4, 5, 2, 6, 1, 5};
         System.out.println(Arrays.toString(arr));
         int newArr[] = new int[size];
-        int length = deleteRepeats(arr, newArr, size);
+        int length = Algorithm.deleteRepeats(arr, newArr, size);
         for (int i = 0; i < length; i++) {
             System.out.print(newArr[i] + " ");
         }
-    }
-
-    public static int deleteRepeats(int[] arr, int[] temp, int n) {
-        int length = 0;
-        for (int i = 0; i < n; i++) {
-            boolean flag = true;
-            for (int j = 0; j < length; j++) {
-                if (temp[j] == arr[i]) {
-                    flag = false;
-                }
-            }
-            if (flag) {
-                temp[length] = arr[i];
-                length++;
-            }
-        }
-        return length;
     }
 }
